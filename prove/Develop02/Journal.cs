3e6c904d2fbcaffce0cd807ad1295ca62b411b56
Radagast02 <lop22011@byui.edu>
2023-05-08 19:29:35 -0600

    Developer Activity Week 3

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -9,7 +9,21 @@ public class Journal
     
     public void WriteEntries()
     {
+         List<string> Prompts = new List<string>()
          
+            {
+                "What was an spiritual impresion you felt today?",
+                "Were you able to feel something while praying?",
+                "How was your mood on your work?",
+                "Did you experience a challenge today? How did you overcome it?",
+                "What can you do to build a stronger family?"
+            };
+            int RandomPrompt = new Random().Next(0, Prompts.Count());
+            string promptSelected = Prompts[RandomPrompt];
+            Console.WriteLine(promptSelected);
+            string answer = Console.ReadLine();
+            Entry  entry = new Entry(DateTime.Now, promptSelected, answer);
+            entry.SaveAsCsvEntry();
     }
 
     public void DisplayEntries()
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,61 @@
+using System;
+
+class Word
+{
+    private Scripture _scripture;
+    private List<string> _scriptureTextList;
+
+    public Word(Scripture scripture)
+    {
+        _scripture = scripture;
+        _scriptureTextList = new List<string>();
+        TexttoList();
+    }
+
+    private void TexttoList()
+    {
+        _scriptureTextList = _scripture.toString().Split(" ").ToList();
+    }
+
+    public void WordsRemover()
+    {
+        int wordsToRemove = new Random().Next(2,4);
+        int wordsRem = 0;
+
+        do
+        {
+            int ramInx = new Random().Next(0, _scriptureTextList.Count());
+            if (_scriptureTextList[ramInx].Contains('_') == false)
+            {
+                _scriptureTextList[ramInx] = new string('_', _scriptureTextList[ramInx].Count());
+                wordsRem++;
+                if (_scriptureTextList.Count() == wordsRem)
+                {
+                    _scriptureTextList[1] = new string('_', _scriptureTextList[0].Count());
+                    wordsRem++;
+                }
+            }
+            
+        }while(wordsRem != wordsToRemove);
+    }
+    public string toString()
+        {
+            return string.Join(' ', _scriptureTextList);
+        }
+
+    public bool NothingLeftOut()
+    {
+        bool value = false;
+
+        foreach(string word in _scriptureTextList)
+        {
+            if (word.Contains('_') == false)
+            {
+                value = true;
+                break;
+            }
+        }
+
+        return value;
+    }
+}
